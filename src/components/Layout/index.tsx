51e08af27f40b815f96194794aeb1a1a857642cf
Stanislav Sava <sava.stanislav@gmail.com>
2021-09-30 16:10:29 +0300

    add ACtion Card component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import * as Dapp from "@elrondnetwork/dapp";
+import { Container } from "react-bootstrap";
 import SignTransactions from "components/SignTransactions";
 import routes, { routeNames } from "routes";
 import NotificationModal from "../NotificationModal";
 import ToastMessages from "../ToastMessages";
 import TransactionSender from "../TransactionSender";
 import TxSubmittedModal from "../TxSubmittedModal";
-import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
@@ -25,7 +25,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="bg-light d-flex flex-column flex-fill wrapper">
       <Navbar />
 
-      <main className="d-flex flex-column flex-grow-1">
+      <main className="d-flex flex-column position-relative flex-grow-1">
         <Sidebar />
         <Dapp.Authenticate routes={routes} unlockRoute={routeNames.unlock}>
           {children}
@@ -36,7 +36,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <ToastMessages />
         </Dapp.Authenticate>
       </main>
-      <Footer />
     </div>
   );
 };
