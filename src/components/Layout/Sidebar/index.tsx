ace4c8a7b09ac212f6416ce0720ba7543a6f9018
Stanislav Sava <sava.stanislav@gmail.com>
2021-09-30 12:14:59 +0300

    update sidebar styles

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -1,52 +1,78 @@
 import React from "react";
+import { faTh } from "@fortawesome/pro-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
 import { useSelector } from "react-redux";
-import { isSidebarOpenSelector } from "../../../redux/selectors/layoutSelector";
+import { isSidebarOpenSelector } from "redux/selectors/layoutSelector";
+
+interface NavigationLinkType {
+  name: string;
+  link: string;
+  icon: IconDefinition;
+}
+
+const links: NavigationLinkType[] = [
+  { name: "Dashboard", link: "#", icon: faTh },
+  { name: "Vault", link: "#", icon: faTh },
+  { name: "Decisions", link: "#", icon: faTh },
+  { name: "Organization", link: "#", icon: faTh },
+  { name: "Tokens", link: "#", icon: faTh },
+];
+
+const secondaryLinks: NavigationLinkType[] = [
+  { name: "FAQ", link: "#", icon: faTh },
+  { name: "Documentation", link: "#", icon: faTh },
+  { name: "End session", link: "#", icon: faTh },
+];
 
 function Sidebar() {
   const isSidebarOpen = useSelector(isSidebarOpenSelector);
+  const [selectedLinkName, setSelectedLinkName] = React.useState("");
+
+  const renderLink = (navigationLink: NavigationLinkType) => (
+    <NavigationLink
+      key={navigationLink.name}
+      navigationLink={navigationLink}
+      selected={selectedLinkName === navigationLink.name}
+      onSelectLink={() => setSelectedLinkName(navigationLink.name)}
+    />
+  );
+
   return isSidebarOpen ? (
     <nav id="sidebar">
-      <div className="sidebar-header">
-        <h3> Sidebar</h3>
-      </div>
-
-      <ul className="list-unstyled components">
-        <li>
-          <a className="active" href="#">
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a className="active" href="#">
-            Vault
-          </a>
-        </li>
-        <li>
-          <a className="active" href="#">
-            Decisions
-          </a>
-        </li>
-        <li>
-          <a className="active" href="#">
-            Organization
-          </a>
-        </li>
-
-        <li>
-          <a href="#">Tokens</a>
-        </li>
-        <li>
-          <a href="#">FAQ</a>
-        </li>
-        <li>
-          <a href="#">Documentation</a>
-        </li>
-        <li>
-          <a href="#">End Session</a>
-        </li>
+      <ul className="list components">
+        {links.map(renderLink)}
+        <hr className="my-5" />
+        {secondaryLinks.map(renderLink)}
       </ul>
     </nav>
   ) : null;
+}
+
+function NavigationLink({
+  navigationLink,
+  selected,
+  onSelectLink,
+}: {
+  navigationLink: NavigationLinkType;
+  selected: boolean;
+  onSelectLink: () => void;
+}) {
+  return (
+    <li
+      onClick={onSelectLink}
+      className={clsx("sidebar-navigation-link", { selected: selected })}
+    >
+      <a className="sidebar-link" href={navigationLink.link}>
+        <FontAwesomeIcon
+          icon={navigationLink.icon}
+          className="navigation-link-icon mx-4"
+        />
+        {navigationLink.name}
+      </a>
+    </li>
+  );
 }
 
 export default Sidebar;
