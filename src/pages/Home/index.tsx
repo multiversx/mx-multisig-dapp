2a0373891e314723af6e552f580736d301b8cf91
macbook <solomon.adrian.92@gmail.com>
2021-10-06 12:19:37 +0300

    fix navbar styles | create main class to centtering content h and v

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,16 +4,16 @@ import { dAppName } from "config";
 import { routeNames } from "routes";
 import { ReactComponent as Hero } from "assets/img/home-img.svg";
 import union from "assets/img/Union.svg";
-import shields from "assets/img/shield.svg";
+import shield from "assets/img/shield.svg";
 
 const Home = () => {
   return (
-    <div className="d-flex flex-fill align-items-center container">
+    <div className="main flex-fill align-items-center container">
       <div className="row w-100">
         <div className="connect col-lg-6 col-sm-6 border-0 d-flex align-items-center">
           <div className="cart-connect">
             <figure>
-              <img src={shields} alt="shield-icon" width="64" height="64" />
+              <img src={shield} alt="shield-icon" width="64" height="64" />
             </figure>
             <h2 className="mb-3" data-testid="title">
               Security designed to
@@ -23,8 +23,10 @@ const Home = () => {
               Elrond Multisig is an ideal balance between security
               <br /> and accessibility.
             </p>
-            Enabling clients to move assets seamlessly
-            <br /> and reliably, protected by multi-signature security.
+            <p className="mb-3">
+              Enabling clients to move assets seamlessly
+              <br /> and reliably, protected by multi-signature security.
+            </p>
             <div className="connect-btns d-flex justify-content-between">
               <div className="connect-btn">
                 <Link
@@ -40,7 +42,7 @@ const Home = () => {
               </div>
               <Link
                 to={routeNames.unlock}
-                className="d-flex align-items-center mt-3"
+                className="d-flex align-items-center mt-3 info"
                 data-testid="loginBtn"
               >
                 What is multisig?
