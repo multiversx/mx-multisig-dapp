3b18d0d8470f4edb448aaf6ba2c475d4a40bc477
constantintovisi <constantin.tovisi@elrond.com>
2021-10-19 15:47:08 +0300

    sam stack

---
+++ sam/private-edge-request/index.js
@@ -0,0 +1,16 @@
+const whitelist = [
+  "5.2.213.244", // office RDS
+  "35.158.37.8", // vpn elrond ?
+  "83.103.170.152", // vpn elrond upc
+  "104.248.25.147", // vpn elrond do
+];
+
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+
+  if (whitelist.includes(request.clientIp)) {
+    callback(null, request);
+  } else {
+    callback(true);
+  }
+};
