b977ca043c13037d6e37cee346fae71ade4c9454
constantintovisi <constantin.tovisi@elrond.com>
2021-10-20 12:42:29 +0300

    whitelisted ip

--- sam/private-edge-request/index.js
+++ sam/private-edge-request/index.js
@@ -3,6 +3,7 @@ const whitelist = [
   "35.158.37.8", // vpn elrond ?
   "83.103.170.152", // vpn elrond upc
   "104.248.25.147", // vpn elrond do
+  "82.77.77.115", // stan
 ];
 
 exports.handler = (event, context, callback) => {
